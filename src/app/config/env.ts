ee055c0804b624ee810465761fd863b5c4630b48
md-suhag <abdussalamsuhag104@gmail.com>
2025-07-11 14:50:39 +0600

    add: login system

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -8,6 +8,7 @@ interface EnvConfig {
   NODE_ENV: "development" | "production";
   BCRYPT_SALT_ROUND: string;
   JWT_ACCESS_SECRET: string;
+  JWT_ACCESS_EXPIRES: string;
   SUPER_ADMIN_EMAIL: string;
   SUPER_ADMIN_PASSWORD: string;
 }
@@ -19,6 +20,7 @@ const loadEnvVariables = (): EnvConfig => {
     "NODE_ENV",
     "BCRYPT_SALT_ROUND",
     "JWT_ACCESS_SECRET",
+    "JWT_ACCESS_EXPIRES",
     "SUPER_ADMIN_EMAIL",
     "SUPER_ADMIN_PASSWORD",
   ];
@@ -34,6 +36,7 @@ const loadEnvVariables = (): EnvConfig => {
     NODE_ENV: process.env.NODE_ENV as "development" | "production",
     BCRYPT_SALT_ROUND: process.env.BCRYPT_SALT_ROUND,
     JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET,
+    JWT_ACCESS_EXPIRES: process.env.JWT_ACCESS_EXPIRES,
     SUPER_ADMIN_EMAIL: process.env.SUPER_ADMIN_EMAIL,
     SUPER_ADMIN_PASSWORD: process.env.SUPER_ADMIN_PASSWORD,
   };
---
+++ src/app/modules/auth/auth.controller.ts
@@ -0,0 +1,23 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { NextFunction, Request, Response } from "express";
+import { catchAsync } from "../../utils/catchAsync";
+import { AuthServices } from "./auth.service";
+import { sendResponse } from "../../utils/sendResponse";
+import { StatusCodes } from "http-status-codes";
+
+const credentialsLogin = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const loginInfo = await AuthServices.credentialsLogin(req.body);
+
+    sendResponse(res, {
+      success: true,
+      statusCode: StatusCodes.OK,
+      message: "User Logged In Successfully",
+      data: loginInfo,
+    });
+  }
+);
+
+export const AuthControllers = {
+  credentialsLogin,
+};
