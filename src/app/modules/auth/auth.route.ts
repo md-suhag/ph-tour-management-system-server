efddc45033cc589c8f1be9634a133752633643a5
md-suhag <abdussalamsuhag104@gmail.com>
2025-07-25 12:42:38 +0600

    update: login with ckecking user status

--- src/app/modules/auth/auth.route.ts
+++ src/app/modules/auth/auth.route.ts
@@ -5,6 +5,7 @@ import { Role } from "../user/user.interface";
 import passport from "passport";
 import { validateRequest } from "../../middlewares/validateRequest";
 import { setPasswordZodSchema } from "./auth.validation";
+import { envVars } from "../../config/env";
 
 const router = Router();
 
@@ -40,7 +41,9 @@ router.get(
 
 router.get(
   "/google/callback",
-  passport.authenticate("google", { failureRedirect: "/login" }),
+  passport.authenticate("google", {
+    failureRedirect: `${envVars.FRONTEND_URL}/login?error=There is some issues with your account. Please contact with out support team!`,
+  }),
   AuthControllers.googleCallbackController
 );
 
