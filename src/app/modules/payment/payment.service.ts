0414510b8d5fab2b64e70b2a146763df3cf96194
md-suhag <abdussalamsuhag104@gmail.com>
2025-07-27 17:44:39 +0600

    feat: sending invoice after successful payment

--- src/app/modules/payment/payment.service.ts
+++ src/app/modules/payment/payment.service.ts
@@ -7,6 +7,10 @@ import { PAYMENT_STATUS } from "./payment.interface";
 import { Payment } from "./payment.model";
 import { ISSLCommerz } from "../sslCommerz/sslCommerz.interface";
 import { SSLService } from "../sslCommerz/sslCommerz.service";
+import { generatePdf, IInoviceData } from "../../utils/invoice";
+import { ITour } from "../tour/tour.interface";
+import { IUser } from "../user/user.interface";
+import { sendEmail } from "../../utils/sendEmail";
 
 const initPayment = async (bookingId: string) => {
   const payment = await Payment.findOne({
@@ -54,12 +58,45 @@ const successPayment = async (query: Record<string, string>) => {
       },
       { new: true, runValidators: true, session: session }
     );
+    if (!updatedPayment) {
+      throw new AppError(401, "Payment not found");
+    }
 
-    await Booking.findByIdAndUpdate(
+    const updatedBooking = await Booking.findByIdAndUpdate(
       updatedPayment?.booking,
       { status: BOOKING_STATUS.COMPLETE },
-      { runValidators: true, session }
-    );
+      { new: true, runValidators: true, session }
+    )
+      .populate("tour", "title")
+      .populate("user", "name email");
+
+    if (!updatedBooking) {
+      throw new AppError(401, "Booking not found");
+    }
+
+    const invoiceData: IInoviceData = {
+      bookingDate: updatedBooking?.createdAt as Date,
+      guestCount: updatedBooking.guestCount,
+      totalAmount: updatedPayment.amount,
+      tourTitle: (updatedBooking?.tour as unknown as ITour).title,
+      transactionId: updatedPayment.transactionId,
+      userName: (updatedBooking?.user as unknown as IUser).name,
+    };
+    const pdfBuffer = await generatePdf(invoiceData);
+
+    await sendEmail({
+      to: (updatedBooking.user as unknown as IUser).email,
+      subject: "Your Tour Booking Invoice",
+      templateName: "invoice",
+      templateData: invoiceData,
+      attachments: [
+        {
+          filename: "invoice.pdf",
+          content: pdfBuffer,
+          contentType: "application/pdf",
+        },
+      ],
+    });
 
     await session.commitTransaction();
     session.endSession();
