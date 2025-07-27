782402cc77c1908422e04c4ecfc91609e6fb3290
md-suhag <abdussalamsuhag104@gmail.com>
2025-07-27 10:13:11 +0600

    feat: user verification using otp

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -5,6 +5,7 @@ import { divisionRoutes } from "../modules/division/division.route";
 import { tourRoutes } from "../modules/tour/tour.route";
 import { bookingRoutes } from "../modules/booking/booking.route";
 import { paymentRoutes } from "../modules/payment/payment.route";
+import { otpRoutes } from "../modules/otp/otp.route";
 
 export const router = Router();
 
@@ -32,6 +33,10 @@ const moduleRoutes = [
   {
     path: "/payment",
     route: paymentRoutes,
+  },
+  {
+    path: "/otp",
+    route: otpRoutes,
   },
 ];
 moduleRoutes.forEach((route) => {
