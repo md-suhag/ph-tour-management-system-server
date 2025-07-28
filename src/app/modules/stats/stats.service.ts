88eb57eb8c6c93b069f9fd72fbb25e1c514cd14d
md-suhag <abdussalamsuhag104@gmail.com>
2025-07-28 10:41:01 +0600

    feat: add payment stats API for admin dashboard

--- src/app/modules/stats/stats.service.ts
+++ src/app/modules/stats/stats.service.ts
@@ -1,5 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Booking } from "../booking/booking.model";
+import { PAYMENT_STATUS } from "../payment/payment.interface";
+import { Payment } from "../payment/payment.model";
 import { Tour } from "../tour/tour.model";
 import { IsActive } from "../user/user.interface";
 import { User } from "../user/user.model";
@@ -287,7 +289,69 @@ const getBookingStats = async () => {
     totalBookingByUniqueUsers,
   };
 };
-const getPaymentStats = async () => {};
+const getPaymentStats = async () => {
+  const totalPaymentPromise = Payment.countDocuments();
+
+  const totalPaymentByStatusPromise = Payment.aggregate([
+    {
+      $group: {
+        _id: "$status",
+        count: { $sum: 1 },
+      },
+    },
+  ]);
+
+  const totalRevenuePromise = Payment.aggregate([
+    {
+      $match: { status: PAYMENT_STATUS.PAID },
+    },
+    {
+      $group: {
+        _id: null,
+        totalRevenue: { $sum: "$amount" },
+      },
+    },
+  ]);
+
+  const avgPaymentAmountPromise = Payment.aggregate([
+    {
+      $group: {
+        _id: null,
+        avgPaymentAMount: { $avg: "$amount" },
+      },
+    },
+  ]);
+
+  const paymentGatewayDataPromise = Payment.aggregate([
+    {
+      $group: {
+        _id: { $ifNull: ["$paymentGatewayData.status", "UNKNOWN"] },
+        count: { $sum: 1 },
+      },
+    },
+  ]);
+
+  const [
+    totalPayment,
+    totalPaymentByStatus,
+    totalRevenue,
+    avgPaymentAmount,
+    paymentGatewayData,
+  ] = await Promise.all([
+    totalPaymentPromise,
+    totalPaymentByStatusPromise,
+    totalRevenuePromise,
+    avgPaymentAmountPromise,
+    paymentGatewayDataPromise,
+  ]);
+  return {
+    totalPayment,
+    totalPaymentByStatus,
+    totalRevenue,
+    avgPaymentAmount,
+    paymentGatewayData,
+  };
+};
 
 export const StatsService = {
   getBookingStats,
