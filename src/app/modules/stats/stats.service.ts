51343d72ec9ee0c34887b778c3132fb50853a6c3
md-suhag <abdussalamsuhag104@gmail.com>
2025-07-28 10:29:28 +0600

    feat: add booking stats API for admin dashboard

--- src/app/modules/stats/stats.service.ts
+++ src/app/modules/stats/stats.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Booking } from "../booking/booking.model";
 import { Tour } from "../tour/tour.model";
 import { IsActive } from "../user/user.interface";
@@ -187,7 +188,105 @@ const getTourStats = async () => {
     totalHighestBookedTour,
   };
 };
-const getBookingStats = async () => {};
+const getBookingStats = async () => {
+  const totalBookingPromise = Booking.countDocuments();
+
+  const totalBookingByStatusPromise = Booking.aggregate([
+    {
+      $group: {
+        _id: "$status",
+        count: { $sum: 1 },
+      },
+    },
+  ]);
+
+  const bookingsPerTourPromise = Booking.aggregate([
+    {
+      $group: {
+        _id: "$tour",
+        bookingCount: { $sum: 1 },
+      },
+    },
+
+    {
+      $sort: { bookingCount: -1 },
+    },
+
+    {
+      $limit: 10,
+    },
+
+    {
+      $lookup: {
+        from: "tours",
+        localField: "_id",
+        foreignField: "_id",
+        as: "tour",
+      },
+    },
+
+    {
+      $unwind: "$tour",
+    },
+
+    {
+      $project: {
+        bookingCount: 1,
+        _id: 1,
+        "tour.title": 1,
+        "tour.slug": 1,
+      },
+    },
+  ]);
+
+  const avgGuestCountPerBookingPromise = Booking.aggregate([
+    {
+      $group: {
+        _id: null,
+        avgGuestCount: { $avg: "$guestCount" },
+      },
+    },
+  ]);
+
+  const bookingsLast7DaysPromise = Booking.countDocuments({
+    createdAt: { $gte: sevenDaysAgo },
+  });
+  const bookingsLast30DaysPromise = Booking.countDocuments({
+    createdAt: { $gte: thirtyDaysAgo },
+  });
+
+  const totalBookingByUniqueUsersPromise = Booking.distinct("user").then(
+    (user: any) => user.length
+  );
+
+  const [
+    totalBooking,
+    totalBookingByStatus,
+    bookingsPerTour,
+    avgGuestCountPerBooking,
+    bookingsLast7Days,
+    bookingsLast30Days,
+    totalBookingByUniqueUsers,
+  ] = await Promise.all([
+    totalBookingPromise,
+    totalBookingByStatusPromise,
+    bookingsPerTourPromise,
+    avgGuestCountPerBookingPromise,
+    bookingsLast7DaysPromise,
+    bookingsLast30DaysPromise,
+    totalBookingByUniqueUsersPromise,
+  ]);
+
+  return {
+    totalBooking,
+    totalBookingByStatus,
+    bookingsPerTour,
+    avgGuestCountPerBooking: avgGuestCountPerBooking[0].avgGuestCount,
+    bookingsLast7Days,
+    bookingsLast30Days,
+    totalBookingByUniqueUsers,
+  };
+};
 const getPaymentStats = async () => {};
 
 export const StatsService = {
